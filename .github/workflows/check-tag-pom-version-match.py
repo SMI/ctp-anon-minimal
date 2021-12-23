2e7f021269e1dd98b37ae0f01e2d77d16d6c881f
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-12-23 20:12:21 +0000

    add check script for deploy

---
+++ .github/workflows/check-tag-pom-version-match.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import os
+import sys
+import xml.etree.ElementTree as ElementTree
+
+_NAMESPACE = "{http://maven.apache.org/POM/4.0.0}"
+
+
+def main() -> int:
+
+    tag_name = os.environ["GITHUB_REF_NAME"]
+
+    pom_root = ElementTree.parse("pom.xml").getroot()
+    pom_version = pom_root.findtext(f"{_NAMESPACE}version")
+    assert pom_version, "Expected to find the pom version"
+
+    if pom_version != tag_name:
+        print(
+            (
+                "Git tag and pom versions do not match. Did you forget to run "
+                "scripts/set-versions.bash?"
+            ),
+            file=sys.stderr,
+        )
+        return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
